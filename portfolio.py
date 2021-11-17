2e25e08fb9fdadb3f8616af88af9d6e972b44869
RosieGZ <93166801+RosieGZ@users.noreply.github.com>
2021-11-17 18:38:45 -0500

    update share class

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,16 @@
+
+class Shares:
+    def __init__(self, name, number, price):
+        self.name = name
+        self.number = number
+        self.price = price
+
 class Portfolio:
     def __init__(self):
         self._stocks = []
 
-    def buy(self, name, shares, price):
-        self._stocks.append((name, shares, price))
+    def buy(self, shares):
+        self._stocks.append(shares)
     
     def cost(self):
-        return sum(shares * price for _, shares, price in self._stocks)
+        return sum(shares.number * shares.price for shares in self._stocks)
--- test_portfolio.py
+++ test_portfolio.py
@@ -4,7 +4,8 @@ from portfolio import Portfolio
 
 def test_buy_one_stock():
     p = Portfolio()
-    p.buy("IBM", 100, 176.48)
+    ibm = Shares("IBM", 100, 176.48)
+    p.buy(ibm)
     assert p.cost() == 17648
 
 def test_empty_portfolio():
@@ -13,8 +14,10 @@ def test_empty_portfolio():
 
 def test_buy_two_stocks():
     p = Portfolio()
-    p.buy("IBM", 100, 176.48)
-    p.buy("HPQ", 100, 36.15)
+    ibm = Shares("IBM", 100, 176.48)
+    hpq = Shares("HPQ", 100, 36.15)
+    p.buy(ibm)
+    p.buy(hpq)
     assert p.cost() == 21263.00
 
 def test_not_enough_arguments_to_buy():
